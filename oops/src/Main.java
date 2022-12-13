3d73161afccb955432e08f9d27a00464a248a5a7
Hemanth <hemanth@hemsolutions.com>
2022-12-13 12:28:35 +0530

    Classes and objects final

--- oops/src/Main.java
+++ oops/src/Main.java
@@ -1,12 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        BankAccount Hemanth = new BankAccount();
-        Hemanth.setAccountHolderName("Hemanth");
-        Hemanth.setAccountBalance(1000);
-        Hemanth.setEmail("hemanth@co.in");
-        Hemanth.setPhoneNumber(96661329);
-        Hemanth.setAccountNumber(201197);
-        Hemanth.depositfunds(100);
-        Hemanth.withdrawfunds(900);
+     //   BankAccount Hemanth = new BankAccount();
+       // Hemanth.setAccountHolderName("Hemanth");
+       // Hemanth.setAccountBalance(1000);
+        // Hemanth.setEmail("hemanth@co.in");
+       /// Hemanth.setPhoneNumber(96661329);
+        /// Hemanth.setAccountNumber(201197);
+        // Hemanth.depositfunds(100);
+        // Hemanth.withdrawfunds(900);
+        SimpleCalculator calculator = new SimpleCalculator();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+        System.out.println("add= " + calculator.getAdditionResult());
+        System.out.println("subtract= " + calculator.getSubtractionResult());
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+        System.out.println("multiply= " + calculator.getMultiplicationResult());
+        System.out.println("divide= " + calculator.getDivisionResult());
     }
 }
--- oops/src/SimpleCalculator.java
+++ oops/src/SimpleCalculator.java
@@ -1,9 +1,41 @@
 public class SimpleCalculator {
-    private int firstNumber;
-    private int lastNumber;
+    private double firstNumber;
+    private double secondNumber;
 
-    public int getfirstNumber(int firstnumber){
-        firstNumber = firstnumber;
-        return firstnumber;
+    public double getAdditionResult(){
+        double result = firstNumber+ secondNumber;
+        return result;
+    }
+    public double getSubtractionResult(){
+        double result = firstNumber- secondNumber;
+        return result;
+    }
+    public double getMultiplicationResult(){
+        double result = firstNumber* secondNumber;
+        return result;
+    }
+    public double getDivisionResult(){
+        if(secondNumber == 0){
+            return 0;
+        }
+        else {
+            double result = firstNumber / secondNumber;
+            return result;
+        }
+    }
+
+    public void setFirstNumber(double firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public double getSecondNumber() {
+        return secondNumber;
+    }
+
+    public void setSecondNumber(double secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+    public double getFirstNumber(){
+        return firstNumber;
     }
 }
